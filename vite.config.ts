8c6ba9e4fb3da7347676776b0e4d6c18ca04a185
xpf <pfxuxpf@163.com>
2026-01-31 16:51:14 +0800

    重新加上base

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({}) => {
+export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-    //base: command === "build" ? "/suda-gs-ams/" : "/",
+    base: command === "build" ? "/suda-gs-ams/" : "/",
   };
 });
